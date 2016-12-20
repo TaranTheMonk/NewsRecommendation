8c4f7db4205d0bd062a38dfff2fec4991f25165c
Jin Tianxing <tianxingjin@JindeMacBook-Pro.local>
2016-12-21 01:54:12 +0800

    Added initialize script

---
+++ initialize.py
@@ -0,0 +1,12 @@
+import RecommendationSys.src.CosSimilarity as cos_sim
+import RecommendationSys.src.BuildInitialMatrix as build_pq_matrix
+import RecommendationSys.src.UpdateMatrix as update_matrix
+from pathlib import Path
+import os
+
+cos_sim.main()
+build_pq_matrix.main()
+if not Path(os.path.extenduser('~/.recsys/Data/Input/input.csv')).is_file():
+	os.system('mkdir -p ~/.recsys/Data/Input')
+	os.system('cat '' > ~/.recsys/Data/Input/input.csv')
+update_matrix.main()
