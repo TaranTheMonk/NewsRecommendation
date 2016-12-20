974216f7eac65f80c02274568afb1e39ed966ce4
root <root@l-dev.recsys1.corp.nestia.com>
2016-12-21 03:16:33 +0800

    Removed test code

--- RecommendationSys/src/Main.py
+++ RecommendationSys/src/Main.py
@@ -442,11 +442,11 @@ def main():
     docslist['cn'].update({'chinese_default': [[['empty', 0]]]*28})
     docslist['en'].update({'english_default': [[['empty', 0]]]*28})
 #output = {'en': '', 'cn': ''}
-    #output_cn = DocsGive(en_dict, Prob_en, docslist['en'], 35, 100)
-    #output_en = DocsGive(cn_dict, Prob_cn, docslist['cn'], 35, 100)
+    output_cn = DocsGive(en_dict, Prob_en, docslist['en'], 35, 100)
+    output_en = DocsGive(cn_dict, Prob_cn, docslist['cn'], 35, 100)
 
-    output_cn = DocsGive(cn_dict, {'chinese_default': Prob_cn['chinese_default']}, docslist['cn'], 35, 100)
-    output_en = DocsGive(en_dict, {'english_default': Prob_en['english_default']}, docslist['en'], 35, 100)
+    #output_cn = DocsGive(cn_dict, {'chinese_default': Prob_cn['chinese_default']}, docslist['cn'], 35, 100)
+    #output_en = DocsGive(en_dict, {'english_default': Prob_en['english_default']}, docslist['en'], 35, 100)
     print('output finished')
     print(time.strftime('%Y-%m-%d %X', time.localtime()))
 ##Test id en: 12d377e804a308f6
