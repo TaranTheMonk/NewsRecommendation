62f3714711dc7e8b35be7ba755ed1edfb65726b7
root <root@l-dev.recsys1.corp.nestia.com>
2016-12-21 02:05:18 +0800

    Finished initialize

--- initialize.py
+++ initialize.py
@@ -4,9 +4,9 @@ import RecommendationSys.src.UpdateMatrix as update_matrix
 from pathlib import Path
 import os
 
-cos_sim.main()
-build_pq_matrix.main()
-if not Path(os.path.extenduser('~/.recsys/Data/Input/input.csv')).is_file():
+#cos_sim.main()
+#build_pq_matrix.main()
+if not Path(os.path.expanduser('~/.recsys/Data/Input/input.csv')).is_file():
 	os.system('mkdir -p ~/.recsys/Data/Input')
 	os.system('cat '' > ~/.recsys/Data/Input/input.csv')
 update_matrix.main()
